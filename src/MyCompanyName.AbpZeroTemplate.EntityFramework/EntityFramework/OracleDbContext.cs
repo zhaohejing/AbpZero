58818c1faa764c52533c78656dc9732c2e6816de
zhaohejing <454258314@qq.com>
2017-03-17 09:01:39 +0800

    ====

---
+++ src/MyCompanyName.AbpZeroTemplate.EntityFramework/EntityFramework/OracleDbContext.cs
@@ -0,0 +1,55 @@
+﻿using Abp.Zero.EntityFramework;
+using MyCompanyName.AbpZeroTemplate.Authorization.Roles;
+using MyCompanyName.AbpZeroTemplate.Authorization.Users;
+using MyCompanyName.AbpZeroTemplate.Chat;
+using MyCompanyName.AbpZeroTemplate.Friendships;
+using MyCompanyName.AbpZeroTemplate.MultiTenancy;
+using MyCompanyName.AbpZeroTemplate.Storage;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCompanyName.AbpZeroTemplate.EntityFramework
+{
+  
+    public class OracleDbContext : AbpZeroDbContext<Tenant, Role, User>
+    {
+        /* Define an IDbSet for each entity of the application */
+
+        public virtual IDbSet<BinaryObject> BinaryObjects { get; set; }
+
+        public virtual IDbSet<Friendship> Friendships { get; set; }
+
+        public virtual IDbSet<ChatMessage> ChatMessages { get; set; }
+
+        /* Setting "Default" to base class helps us when working migration commands on Package Manager Console.
+         * But it may cause problems when working Migrate.exe of EF. ABP works either way.         * 
+         */
+        public OracleDbContext()
+            : base("DefaultOracle")
+        {
+
+        }
+
+        /* This constructor is used by ABP to pass connection string defined in AbpZeroTemplateDataModule.PreInitialize.
+         * Notice that, actually you will not directly create an instance of AbpZeroTemplateDbContext since ABP automatically handles it.
+         */
+        public OracleDbContext(string nameOrConnectionString)
+            : base(nameOrConnectionString)
+        {
+
+        }
+
+        /* This constructor is used in tests to pass a fake/mock connection.
+         */
+        public OracleDbContext(DbConnection dbConnection)
+            : base(dbConnection, true)
+        {
+
+        }
+    }
+}
